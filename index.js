ef0567035861226339a103b5cdfee29b56841037
javier9620 <javier.rgz_96@hotmail.com>
2024-01-14 21:05:34 -0600

    Index code

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+let token = "";
+let fullToken = "";
+
+app.use(express.json());
+app.use(cors());
+const PORT = 8080;
+
+app.get("/TOKK/", (req, res) => {
+  token = req.query.code;
+  fullToken = `Bearer ${token}`;
+  console.log(fullToken);
+  res.json("took");
+});
+
+app.get("/fullTokk", (req, res) => {
+  console.log(fullToken);
+  res.send(JSON.stringify({ token, fullToken }));
+});
+
+app.listen(PORT, () => {
+  console.log("Server listerning on Port localhost:" + PORT);
+});
